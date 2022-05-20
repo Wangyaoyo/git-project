1067e342cf06af4b1ccd5f07ee7f5bf50ee1a657
wangyao <18229028903@163.com>
2022-05-20 09:30:01 +0800

    fouth  commit

--- src/main/java/org/dao/UserDao.java
+++ src/main/java/org/dao/UserDao.java
@@ -9,5 +9,6 @@ public class UserDao {
     public static void main(String[] args) {
         System.out.println("11111");
         System.out.println("hot-fix");
+        System.out.println("master");
     }
 }
